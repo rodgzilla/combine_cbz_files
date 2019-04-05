59af9196066f68705fa80850577c9094111dbcc9
Grégory Châtel <chatel.gregory@gmail.com>
2019-04-05 19:47:27 +0200

    Proper combine all (with chapter ordering).

--- combine_all_tomes.py
+++ combine_all_tomes.py
@@ -7,13 +7,27 @@ from collections import defaultdict
 def create_archive(dest_filename, filenames):
     with tempfile.TemporaryDirectory() as tmp_folder:
         tmp_folder_path = pathlib.Path(tmp_folder)
+        # Copy all cbz files to a temporary directory
         for filename in filenames:
             shutil.copy(filename, tmp_folder_path / filename)
-        for filename in tmp_folder_path.iterdir():
+        # Extract all the file and rename the file that were in the
+        # archive. The original cbz file should be ordered
+        # lexicographically
+        i = 0
+        for filename in sorted(tmp_folder_path.iterdir()):
             if filename.suffix != '.cbz':
                 continue
+            # Extracting the files
             with ZipFile(filename, 'r') as zip_file:
-                zip_file.extractall(tmp_folder_path)
+                image_filenames = zip_file.namelist()
+                res             = zip_file.extractall(tmp_folder_path)
+            # Renaming the resulting files
+            for image_fn in image_filenames:
+                p = tmp_folder_path / image_fn
+                p.rename(tmp_folder_path / f'p_{i:05d}.jpg')
+                i += 1
+
+        # Creating a new cbz file combining all the extracts
         with ZipFile(dest_filename, 'w') as dest_zip_file:
             for filename in tmp_folder_path.iterdir():
                 if filename.suffix != '.jpg':
@@ -28,6 +42,8 @@ def treat_current_directory():
 
     volume_to_chapters = defaultdict(list)
 
+    # Building a map between the volume numbers and the
+    # filenames of the cbz files that compose that volume
     for filepath in sorted(current_dir.iterdir()):
         if filepath.suffix != '.cbz':
             continue
@@ -35,9 +51,9 @@ def treat_current_directory():
         volume_number = filename.split()[0][-2:]
         volume_to_chapters[volume_number].append(filepath)
 
+    # Create all the volumes, one by one
     for volume in volume_to_chapters:
-        archive_name = f'vol_{volume}.cbz'
-        print(volume, archive_name)
+        archive_name = f'naruto_vol_{volume}.cbz'
         create_archive(
             archive_name,
             volume_to_chapters[volume]
